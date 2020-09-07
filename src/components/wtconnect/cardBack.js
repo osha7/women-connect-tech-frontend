dcf8994afaf0b57a052b88eb7525e63f385082c7
osha7 <ogroetz@gmail.com>
2020-09-06 21:26:33 -0600

    connect cards spin on mouseover

--- src/components/wtconnect/cardBack.js
+++ src/components/wtconnect/cardBack.js
@@ -4,8 +4,10 @@ export default function CardBack(props) {
 
     return (
         <div className="card-back">
+            <span />
             <h5>Goals:</h5><br />
             <p>{props.goals}</p>
+            <span />
         </div>
     )
 }
--- src/components/wtconnect/user.js
+++ src/components/wtconnect/user.js
@@ -9,17 +9,20 @@ export default function User(props) {
             {/* <img className="users-avatar" src={props.avatar.link} alt={props.avatar.id} />
             <br />
             < >{props.first_name}{props.last_name}</> */}
-            <CardFront avatar={props.avatar} first_name={props.first_name} last_name={props.last_name} />
-            <CardBack 
-            goals={props.goals}
-            resume_link={props.resume_link}
-            website={props.website}
-            contact={props.contact}
-            linked_in={props.linked_in}
-            twitter={props.twitter}
-            available_to_work={props.available_to_work}
-            open_to_connect={props.open_to_connect}
-            open_to_mentor={props.open_to_mentor}/>
+            <div className="flip-card-inner">
+                <CardFront avatar={props.avatar} first_name={props.first_name} last_name={props.last_name} />
+                <CardBack 
+                goals={props.goals}
+                resume_link={props.resume_link}
+                website={props.website}
+                contact={props.contact}
+                linked_in={props.linked_in}
+                twitter={props.twitter}
+                available_to_work={props.available_to_work}
+                open_to_connect={props.open_to_connect}
+                open_to_mentor={props.open_to_mentor}
+                />
+            </div>
         </div>
     )
 }
